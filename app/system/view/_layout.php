43c701e203bcdb2ebfe864fa4ceaf597b92d1cad
Kerem Güneş <1508306+okerem@users.noreply.github.com>
2023-08-22 01:24:59 +0300

    Make-ups.

--- app/system/view/_layout.php
+++ app/system/view/_layout.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title><?= page_title() ?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="<?= page_description() ?>">
-    <title><?= page_title() ?></title>
 </head>
 <body>
 
