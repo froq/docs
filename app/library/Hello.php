095fffab1945582f7e2dce3ad7056e946905dcd2
Kerem Güneş <1508306+okerem@users.noreply.github.com>
2023-08-25 23:18:40 +0300

    Drop mock stuff.

--- app/library/Hello.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace app\library;
-
-class Hello {
-    function __construct(public $name) {}
-
-    function greet() { return format('Hello, %s', ($this->name)); }
-}
--- app/library/fun.php
+++ app/library/fun.php
@@ -4,7 +4,7 @@ const PAGE_TITLE = 'Froq! Framework';
 const PAGE_DESCRIPTION = 'Froq! Hassle-free PHP framework.';
 
 function page_title() {
-    $view = app()::registry()::get('@view');
+    $view  = app()::registry()::get('@view');
     $title = $view->getData('title');
 
     return join(' | ', filter([PAGE_TITLE, $title]));
@@ -13,17 +13,3 @@ function page_title() {
 function page_description() {
     return PAGE_DESCRIPTION; // @fornow
 }
-
-// function froq_data($key) {
-//     $view = app()::registry()::get('@view');
-
-//     return $view->getData($key);
-// }
-
-// function froq_layout($name) {
-//     $file = sprintf('%s/app/system/view/_layout_%s.php', APP_DIR, $name);
-
-//     ob_start();
-//     include $file;
-//     return ob_get_clean() . PHP_EOL;
-// }
