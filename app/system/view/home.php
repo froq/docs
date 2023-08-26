e3e38d0677af947afae5aa8ec06d9ef732e674b2
Kerem Güneş <1508306+okerem@users.noreply.github.com>
2023-08-26 21:41:39 +0300

    Update home page docs.

--- app/system/view/home.php
+++ app/system/view/home.php
@@ -1,13 +1,17 @@
 <div class="home">
     <h2>Welcome</h2>
-    <p>Froq! is a PHP framework that helps you quickly write simple yet powerful web applications and APIs with a minimal installation / configuration process. It basically accepts HTTP requests, applies a method / callback routine, and returns HTTP responses.</p>
+
+    <p>Froq! is a PHP framework that helps you quickly write simple yet powerful web applications and APIs with a minimal installation / configuration process. It basically accepts HTTP requests, applies some method / callback routines, and returns HTTP responses.</p>
+
     <p>Froq! neither follows MVC architecture rules nor contains a Model component, but instead, it makes use of Controller, Repository and View components.</p>
+
     <p>Froq! can also be used as a micro framework as you might be familiar with <a href="//slimframework.com/">Slim</a> framework.</p>
 
     <br>
 
     <h2>How does it work?</h2>
-    <p>After applying web server configs, Froq! can be installed and run easily. Here is a <code>HelloController</code> example:</p>
+
+    <p>After applying web server configurations, Froq! can be installed and run easily. Here is a <code>HelloController</code> example:</p>
 
     <pre><code class="language-php">
     // app/config/routes.php
@@ -32,4 +36,8 @@
         echo "Hello, ", escape($name), "!\n";
     });
     </code></pre>
+
+    <br>
+
+    <p><em>Note: The <code>escape()</code> function doesn't exist in Froq! but is used there just because to emphasise that the <code>$name</code> variable is an external input (path / segment parameter).</em></p>
 </div><!-- .home -->
