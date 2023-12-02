b57369e1b52c9d84f158954f3c729c52a649b249
Kerem Güneş <1508306+okerem@users.noreply.github.com>
2023-12-02 22:58:52 +0300

    fun: add test_path() [for side if's].

--- app/library/fun.php
+++ app/library/fun.php
@@ -3,13 +3,25 @@
 const PAGE_TITLE = 'Froq! Framework';
 const PAGE_DESCRIPTION = 'Froq! Hassle-free PHP framework.';
 
-function page_title() {
-    $view  = app()::registry()::get('@view');
+function page_title(): string {
+    $view = app()->registry()::get('@view');
     $title = $view->getData('title');
 
     return join(' | ', filter([PAGE_TITLE, $title]));
 }
 
-function page_description() {
-    return PAGE_DESCRIPTION; // @fornow
+function page_description(): string {
+    return PAGE_DESCRIPTION; // For now?
+}
+
+function test_path(string ...$patterns): bool {
+    $path = app()->request->getPath();
+
+    foreach ($patterns as $pattern) {
+        if (preg_test("~{$pattern}~", $path)) {
+            return true;
+        }
+    }
+
+    return false;
 }
